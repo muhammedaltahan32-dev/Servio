a1487da1f85afdc64993459c4ebe1551f5d873d3
0Tahan0 <wwwkl545@gmail.com>
2026-08-02 14:55:35 +0300

    install material UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import TextField from '@mui/material/TextField';
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </div>
+  );
 };
 
 export default App;
