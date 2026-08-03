238e030c262ef2ef80701c3810eb51406e449cfb
Ahmed Zaatar <151833821+AhmedZaatar@users.noreply.github.com>
2026-08-03 19:52:41 +0300

    Rename Kind_ADMINS to Kind_ADMIN

--- constants/user.js
+++ constants/user.js
@@ -1,3 +1,4 @@
-export const Kind_ADMINS = "admin";
+export const Kind_ADMIN = "admin";
 export const Kind_WAITER = "waiter";
 export const Kind_KITCHEN = "kitchen";
+export const Kind_GUESTS = "guests";
