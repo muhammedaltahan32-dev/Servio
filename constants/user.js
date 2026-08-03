5980ae6e71c081b2f0fcf24ec1dd794b5639efc8
AhmedZaatar <ahmedzaatar1999@gmail.com>
2026-08-03 17:43:27 +0300

    Update user constants file

--- constants/user.js
+++ constants/user.js
@@ -1,5 +1,3 @@
-export const allowedRoles_User = [2, 3]; // 2 for owners and 3 users
-export const Kind_ADMINS = "admins";
-export const Kind_OWNERS = "owners";
-export const Kind_USERS = "users";
-export const Kind_GUESTS = "guests";
+export const Kind_ADMINS = "admin";
+export const Kind_WAITER = "waiter";
+export const Kind_KITCHEN = "kitchen";
