32eab1d17fc8f01505c35856b13a19dec60bc624
AhmedZaatar <ahmedzaatar1999@gmail.com>
2026-08-03 17:38:24 +0300

    Add consts folder to root directory

---
+++ constants/SubApi.js
@@ -0,0 +1,3 @@
+export const Api_Signin = "api/signin";
+export const Api_User = "api/user";
+export const Api_Architecture = "api/architecture";
---
+++ constants/user.js
@@ -0,0 +1,5 @@
+export const allowedRoles_User = [2, 3]; // 2 for owners and 3 users
+export const Kind_ADMINS = "admins";
+export const Kind_OWNERS = "owners";
+export const Kind_USERS = "users";
+export const Kind_GUESTS = "guests";
