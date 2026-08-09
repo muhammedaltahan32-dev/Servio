5f83c5e04c4fd28aec8d02869ea0e38cbb14049b
Ahmed Zaatar <151833821+AhmedZaatar@users.noreply.github.com>
2026-08-09 18:03:26 +0300

    Add allowedRoles_User for user role management

--- constants/user.js
+++ constants/user.js
@@ -1,4 +1,6 @@
+export const allowedRoles_User = [2, 3]; // 2 for waiter and 3 kitchen
 export const Kind_ADMIN = "admin";
 export const Kind_WAITER = "waiter";
 export const Kind_KITCHEN = "kitchen";
 export const Kind_GUESTS = "guests";
+
