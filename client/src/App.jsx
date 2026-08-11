0ee09106e5d1b429d9444dc251aaeb32db108cb3
muhammedaltahan32-dev <muhammedaltahan32@gmail.com>
2026-08-11 18:04:52 +0300

    create login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home, Login } from "@pages";
+import { ProtectedRoute } from "@router";
 
-import { ApiService } from "./services/ApiService.js";
-import axios from "axios";
-export const App = () => {
-	const [isLoading, setLoading] = React.useState(false);
-	const isFetchedRef = React.useRef(null);
-	React.useEffect(() => {
-		if (isFetchedRef.current) return;
-		isFetchedRef.current = (async () => {
-			const response = await ApiService.post("signin", {
-				name: "admin",
-				password: "admin",
-			});
-			if (response) {
-				localStorage.setItem("token", response.token);
-			}
-		})();
-	}, []);
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route element={<ProtectedRoute />}>
+					<Route path="/" element={<Home />} />
+				</Route>
 
-	return <div className="app">{isLoading && "...loading"}</div>;
-};
+				<Route path="*" element={<Navigate to="/login" replace />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
 
 export default App;
