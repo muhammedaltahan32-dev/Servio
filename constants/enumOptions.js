6c7d4e87374cbdb486062b47f20bd5ad55505334
muhammedaltahan32-dev <muhammedaltahan32@gmail.com>
2026-08-12 15:33:23 +0300

    add account enum values

--- constants/enumOptions.js
+++ constants/enumOptions.js
@@ -4,3 +4,7 @@ export const Kind_WAITER = "waiter";
 export const Kind_KITCHEN = "kitchen";
 export const Kind_GUESTS = "guests";
 
+export const KINDS_VALUES = {
+  [Kind_KITCHEN] : 2,
+  [Kind_WAITER] : 3,
+}
