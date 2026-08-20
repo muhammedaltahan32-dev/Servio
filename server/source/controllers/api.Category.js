337e3086e60e92883a088acc0285e8b8142c6f8e
Ahmed Zaatar <151833821+AhmedZaatar@users.noreply.github.com>
2026-08-20 14:09:13 +0300

    Update sorting field for category retrieval

--- server/source/controllers/api.Category.js
+++ server/source/controllers/api.Category.js
@@ -1,13 +1,14 @@
 import { mdlCategories } from "../../../constants/modelNames.js";
 import { Api_Category } from "../../../constants/SubApi.js";
 import { St_BAD_REQUEST, St_CREATED, St_OK } from "../../../constants/HttpStatus.js";
+import { Cat_Sort } from "../../../constants/FieldsName.js";
 
 export const subapi = Api_Category;
 
 export const getAll = async (req, res, params) => {
 	try {
 		const { [mdlCategories]: Category } = req.app.locals.db;
-		const categories = await Category.findAll({ order: [["sort_order", "ASC"]] });
+		const categories = await Category.findAll({ order: [[Cat_Sort, "ASC"]] });
 		res.status(St_OK).json({ success: true, data: categories });
 	} catch (err) {
 		res.status(St_BAD_REQUEST).json({ success: false, error: err.message });
