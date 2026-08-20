6254b2798cc13433d24d0b259d031e915e6bcdeb
Ahmed Zaatar <151833821+AhmedZaatar@users.noreply.github.com>
2026-08-20 14:45:38 +0300

    Refactor user model to table model definition

--- server/source/models/mdl.Table.js
+++ server/source/models/mdl.Table.js
@@ -1,25 +1,36 @@
-import { mdlUser } from "../../../constants/modelNames.js";
-import { User_ID, User_Name, User_HashedPassword, User_Kind, User_IsActive } from "../../../constants/FieldsName.js";
-import { Kind_ADMIN, Kind_KITCHEN, Kind_WAITER } from "../../../constants/enumOptions.js";
+import { mdlTable } from "../../../constants/modelNames.js";
+import {
+	Table_ID,
+	Table_Number,
+	Table_Capacity,
+	Table_Status,
+	Table_UpdatedAt,
+} from "../../../constants/FieldsName.js";
+import { ST_AVAILABLE, ST_NEEDS_CLEANING, ST_OCCUPIED } from "../../../constants/enumOptions.js";
 
-const defineUser = (sequelize, DataTypes) => {
+const defineTables = (sequelize, DataTypes) => {
 	const model = sequelize.define(
-		mdlUser,
+		mdlTable,
 		{
-			[User_ID]: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-			[User_Name]: { type: DataTypes.STRING(40), allowNull: false },
-			[User_HashedPassword]: { type: DataTypes.STRING(200), allowNull: false },
-			[User_Kind]: { type: DataTypes.ENUM(Kind_ADMIN, Kind_KITCHEN, Kind_WAITER), allowNull: false },
-			[User_IsActive]: { type: DataTypes.BOOLEAN },
+			[Table_ID]: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+			[Table_Number]: { type: DataTypes.INTEGER, allowNull: false, unique: true },
+			[Table_Capacity]: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 4 },
+			[Table_Status]: {
+				type: DataTypes.ENUM(ST_AVAILABLE, ST_OCCUPIED, ST_NEEDS_CLEANING),
+				defaultValue: ST_AVAILABLE,
+			},
+			[Table_UpdatedAt]: {
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+			},
 		},
 		{
-			tableName: "user",
+			tableName: "table",
 			timestamps: false,
 		},
 	);
 
-	return [mdlUser, model];
+	return [mdlTable, model];
 };
 
-export default defineUser;
-
+export default defineTables;
