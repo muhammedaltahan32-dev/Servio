fee644bde3b8ec31a3fb21bf3a1f12b1d43efb7d
Ahmed Zaatar <151833821+AhmedZaatar@users.noreply.github.com>
2026-08-20 14:14:16 +0300

    Refactor payment API to use constant field names
    Updated payment processing to use constants for field names.

--- server/source/controllers/api.Payment.js
+++ server/source/controllers/api.Payment.js
@@ -1,6 +1,16 @@
 import { mdlPayments, mdlOrders, mdlTable } from "../../../constants/modelNames.js";
 import { Api_Payment } from "../../../constants/SubApi.js";
 import { St_BAD_REQUEST, St_CREATED, St_INTERNAL_SERVER_ERROR, St_OK } from "../../../constants/HttpStatus.js";
+import {
+	Pay_OrderID,
+	Pay_Method,
+	Pay_Amount,
+	Pay_CreatedAt,
+	Order_TableID,
+	Order_Total,
+	Order_Status,
+	Table_Status,
+} from "../../../constants/FieldsName.js";
 
 export const subapi = Api_Payment;
 
@@ -8,8 +18,8 @@ export const getAll = async (req, res, params) => {
 	try {
 		const { [mdlPayments]: Payment, [mdlOrders]: Order } = req.app.locals.db;
 		const payments = await Payment.findAll({
-			include: [{ model: Order, attributes: ["table_id", "total_amount"] }],
-			order: [["created_at", "DESC"]],
+			include: [{ model: Order, attributes: [Order_TableID, Order_Total] }],
+			order: [[Pay_CreatedAt, "DESC"]],
 		});
 		res.status(St_OK).json({ success: true, data: payments });
 	} catch (err) {
@@ -22,13 +32,13 @@ export const post = async (req, res) => {
 	const t = await sequelize.transaction();
 
 	try {
-		const { order_id, payment_method, amount_paid } = req.body;
+		const { [Pay_OrderID]: order_id, [Pay_Method]: payment_method, [Pay_Amount]: amount_paid } = req.body;
 
 		const newPayment = await Payment.create(
 			{
-				order_id,
-				payment_method,
-				amount_paid,
+				[Pay_OrderID]: order_id,
+				[Pay_Method]: payment_method,
+				[Pay_Amount]: amount_paid,
 			},
 			{ transaction: t },
 		);
@@ -36,12 +46,12 @@ export const post = async (req, res) => {
 		const order = await Order.findByPk(order_id, { transaction: t });
 		if (!order) throw new Error("Order not found");
 
-		order.status = "PAID";
+		order[Order_Status] = "PAID";
 		await order.save({ transaction: t });
 
-		const table = await Table.findByPk(order.table_id, { transaction: t });
+		const table = await Table.findByPk(order[Order_TableID], { transaction: t });
 		if (table) {
-			table.status = "NEEDS_CLEANING";
+			table[Table_Status] = "NEEDS_CLEANING";
 			await table.save({ transaction: t });
 		}
 
