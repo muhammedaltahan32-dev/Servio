aee6fdcecb8e8d5ced21254bd826a0e3e667886b
muhammedaltahan32-dev <muhammedaltahan32@gmail.com>
2026-08-27 14:08:38 +0300

    create page container

---
+++ client/src/components/Container/PageContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container as MUContainer } from "@mui/material";
+const EMPTY_OBJECT = {};
+export const PageContainer = React.forwardRef(({ children, sx = EMPTY_OBJECT, ...props }, ref) => {
+	return (
+		<MUContainer
+			ref={ref}
+			maxWidth="xl"
+			sx={{ py: 5, height: "100%", flex: 1, display: "flex", flexDirection: "column", ...sx }}
+			{...props}
+		>
+			{children}
+		</MUContainer>
+	);
+});
+export default PageContainer;
+PageContainer.displayName = "pageContainer";
