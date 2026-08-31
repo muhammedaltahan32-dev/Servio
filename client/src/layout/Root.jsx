897dc79f64422448e57cbe8ff631a50898f82386
muhammedaltahan32-dev <muhammedaltahan32@gmail.com>
2026-08-31 16:10:30 +0300

    improve layout

--- client/src/layout/Root.jsx
+++ client/src/layout/Root.jsx
@@ -3,8 +3,14 @@ import AppBar from "./Appbar/AppBar.jsx";
 import { Box } from "@mui/material";
 import SideBar from "./Sidebar/SideBar.jsx";
 import Main from "./Main/Main.jsx";
+import { useDispatch } from "react-redux";
 
 export default function Root() {
+	const dispatch = useDispatch();
+
+	React.useEffect(() => {
+		dispatch({ type: "socket/init" });
+	}, [dispatch]);
 	return (
 		<Box sx={{ display: "flex", overflow: "auto" }}>
 			<AppBar />
