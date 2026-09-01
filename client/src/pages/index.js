f7ec43399a0721f120eb99072ecde7fabec5815d
muhammedaltahan32-dev <muhammedaltahan32@gmail.com>
2026-09-01 14:50:24 +0300

    remove Test page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,6 @@ export * from "./login/Login.jsx";
 export * from "./home/Home.jsx";
 export * from "./notfound/NotFound.jsx";
 export * from "./categories/CategoriesPage.jsx";
-export * from "./Test.jsx";
 export * from "./Lobby/LobbyPage.jsx";
 export * from "./tables/TablesPage.jsx";
 export * from "./menuItems/MenuItemsPage.jsx";
