44d489ac05dcd20ecc50421e0ed18cf8685c9926
muhammedaltahan32-dev <muhammedaltahan32@gmail.com>
2026-09-06 17:04:28 +0300

    fix content boundries

--- client/src/components/Container/PageContainer.jsx
+++ client/src/components/Container/PageContainer.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import { Container as MUContainer } from "@mui/material";
 const EMPTY_OBJECT = {};
 export const PageContainer = React.forwardRef(({ children, sx = EMPTY_OBJECT, ...props }, ref) => {
+	const resolvedSX = React.useCallback(
+		(theme) => {
+			let overrideStyles = sx;
+			if (typeof overrideStyles === "function") overrideStyles = overrideStyles(theme);
+			return {
+				height: "100%",
+				flex: 1,
+				p: { md: 3, sm: 2.5, xs: 2 },
+				display: "flex",
+				flexDirection: "column",
+				...overrideStyles,
+			};
+		},
+		[sx],
+	);
 	return (
-		<MUContainer
-			ref={ref}
-			maxWidth="xl"
-			disableGutters
-			sx={{ height: "100%", flex: 1,p:0, display: "flex", flexDirection: "column", ...sx }}
-			{...props}
-		>
+		<MUContainer ref={ref} maxWidth="xl" disableGutters sx={resolvedSX} {...props}>
 			{children}
 		</MUContainer>
 	);
